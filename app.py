10faa65d4fb070c811f09f17cb528bfe9a0024c9
Paramfpv <110547922+Paramfpv@users.noreply.github.com>
2024-11-04 20:52:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import wikipedia
+
 import streamlit as st
 
 st.title("Wiki Summarizer")
-text=st.text_input("enter the name of the page to be summarized")
-sm = (wikipedia.page(text).summary)
-st.write("SUMMARY : ", sm)
+#text=st.text_input("enter the name of the page to be summarized")
+#sm = (wikipedia.page(text).summary)
+#st.write("SUMMARY : ", sm)
